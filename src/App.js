789d5bfe3372cc3d260f76a1913e29952fa0c4bf
Samith-Sankalpa <sankalpa.gss@gmail.com>
2021-07-20 16:09:24 +0530

    uploaded

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Users from "./components/users";
 
-function App() {
+export default function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((json) => setUsers(json));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <h1>Fetching data from third party API</h1>
+      </div>
+      <div>
+        <Users data={users} />
+      </div>
+    </>
   );
 }
-
-export default App;
